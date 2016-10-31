d1c817d5d404d62ce318ad58009effc9f68db8fc
lki1354 <lki1354@students.fhv.at>
2016-10-31 13:16:18 +0100

    add answers of the quiz until we start to build my resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+$("#main").append(["Lukas<br>"]);
+
+c = 299792458; // meters/second
+mTOcm = 100; // centimeters
+nsTOs = 1.0/1000000000; // seconds
+// light in centimeters/nanosecond
+cCMpNS= c*mTOcm*nsTOs;
+console.log(cCMpNS);
+
+
+
+var awesomeThoughts;
+awesomeThoughts = "I am Lukas and I am AWESOME!";
+console.log(awesomeThoughts);
+
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
+
+
